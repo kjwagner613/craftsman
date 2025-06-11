6953215416712b6935b4d8b63620fef4a2c5fd99
kjwagner613 <kjwagner613@gmail.com>
2025-06-11 07:55:27 -0700

    updated pillar transparent click zones

--- src/assets/components/Pillars.jsx
+++ src/assets/components/Pillars.jsx
@@ -12,14 +12,14 @@ const Pillars = ({ setSelectedSection }) => {
     return (
         <div className={`transition-opacity duration-1000 ${isVisible ? "opacity-100" : "opacity-0"} relative`}>
             {/* Pillar Image */}
-            <img src="/pillars3.png" alt="Business Pillars" className="w-[700px] z-10" />
-            <div className="cloud"></div>
+            <div className="cloud z-5"></div>
+            <img src="/pillars3.png" alt="Business Pillars" className="max-w-[700px] z-10" />
 
             {/* Transparent Click Areas */}
-            <div className="absolute top-[86.5%] left-[3%] w-[670px] h-[35px] cursor-pointer" onClick={() => setSelectedSection("foundation")}></div>
-            <div className="absolute top-[30%] left-[10%] w-[130px] h-[250px] cursor-pointer" onClick={() => setSelectedSection("software")}></div>
-            <div className="absolute top-[30%] left-[41%] w-[130px] h-[250px] cursor-pointer" onClick={() => setSelectedSection("projectManagement")}></div>
-            <div className="absolute top-[30%] left-[72%] w-[130px] h-[250px] cursor-pointer" onClick={() => setSelectedSection("automation")}></div>
+            <div className="absolute top-[86.5%] left-[3%] w-[600px] h-[35px] cursor-pointer" onClick={() => setSelectedSection("foundation")}></div>
+            <div className="absolute top-[30%] left-[10%] w-[120px] h-[225px] cursor-pointer" onClick={() => setSelectedSection("software")}></div>
+            <div className="absolute top-[30%] left-[41%] w-[120px] h-[225px] cursor-pointer" onClick={() => setSelectedSection("projectManagement")}></div>
+            <div className="absolute top-[30%] left-[72%] w-[120px] h-[225px] cursor-pointer" onClick={() => setSelectedSection("automation")}></div>
         </div>
     );
 };

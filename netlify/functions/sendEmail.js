6c2ffa9d1ff75a08d22bbe1e869043365b99b702
kjwagner613 <kjwagner613@gmail.com>
2025-06-21 14:43:12 -0700

    test

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -22,8 +22,8 @@ exports.handler = async (event) => {
   }
 
   const msg = {
-    to: process.env.TO_EMAIL,
-    from: process.env.FROM_EMAIL,
+    to: "coderwagner@gmail.com",
+    from: "coderwagner@gmail.com",
     subject: `New message from ${senderName}`,
     text: message,
     html: `<p><strong>From:</strong> ${senderName} (${email})</p><p>${message}</p>`,

a785c42084509e67f9c9745ed5a600293b0a5f81
Kevin Wagner <kjwagner613@gmail.com>
2025-10-05 08:51:06 -0700

    new field link size

--- src/utils/grids.js
+++ src/utils/grids.js
@@ -1,4 +1,4 @@
 export const grids = {
-  contactTiles: "grid grid-cols-3 gap-x-[10px] gap-y-[10px] mb-[20px] w-full",
-  contactFormGrid: "grid grid-cols-2 gap-[50px]",
+  contactTiles: "grid grid-cols-1 min-[480px]:grid-cols-2 min-[768px]:grid-cols-3 gap-x-[10px] gap-y-[10px] mb-[20px] w-full",
+  contactFormGrid: "grid grid-cols-1 sm:grid-cols-2 gap-[20px] sm:gap-[50px]",
 };
